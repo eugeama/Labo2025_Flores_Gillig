9fe7e974205a6d7c9194d87cb4e0d4461a6e9841
eugeama <amaliagillig2508@gmail.com>
2025-04-07 10:25:19 -0300

    hola

---
+++ IntroducirNumero.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class IntroducirNumero {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        int cantidad = 0;
+        int numero = 0;
+        while (numero != -1) {
+            System.out.println("Ingrese un numero: ");
+            numero = e.nextInt();
+            if(numero !=-1) {
+                cantidad++;
+            }
+        }
+        System.out.println(cantidad);
+    }
+}
---
+++ PalabrasIguales.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PalabrasIguales {
+    public static void main(String[] args) {
+        Scanner e = new Scanner(System.in);
+        System.out.println("Ingrese la primera palabra: ");
+        String palabra1= e.nextLine();
+        System.out.println("Ingrese la segunda palabra: ");
+        String palabra2= e.nextLine();
+
+        if(palabra1.equals(palabra2)){
+            System.out.println("Son iguales");
+        }
+        else {
+            System.out.println("No son iguales");
+        }
+    }
+}
