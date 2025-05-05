5604bced07aaf58134181dffd506fd244c196626
tobiama <amaliagillig2508@gmail.com>
2025-05-05 00:34:29 -0300

    Ejercicios nuevos Clothing

--- ejerciciosCursoFlores/Clothing.java
+++ ejerciciosCursoFlores/Clothing.java
@@ -4,6 +4,8 @@ public class Clothing{
     String descripcion;
     double precio;
     String talle;
+    private final double minPrice= 10.0;
+    private final double minTax= 0.2;
 
     public Clothing() {
         this.talle= "M";
@@ -12,5 +14,29 @@ public class Clothing{
         this.descripcion = descripcion;
         this.precio = precio;
         this.talle = talle;
+    }
+
+    public String getDescription() {
+        return descripcion;
+    }
+
+    public void setDescription(String desc) {
+        this.descripcion = desc;
+    }
+
+    public double getPrecio() {
+        return precio + (precio*minTax);
+    }
+
+    public void setPrice(double price) {
+        this.precio = Math.max(price, minPrice);
+    }
+
+    public String getSize() {
+        return talle;
+    }
+
+    public void setSize(String size) {
+        this.talle = size;
     }
 }
