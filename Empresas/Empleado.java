1613bc85da1f6f21aa368d855f6659e1f54048cf
tobiama <amaliagillig2508@gmail.com>
2025-06-17 23:22:04 -0300

    Delete Empresas directory

--- Empresas/Empleado.java
+++
@@ -1,9 +0,0 @@
-package Empresas;
-
-public class Empleado {
-    private String nombre;
-    private String apellido;
-    private int DNI;
-    private String pais;
-    private int telefono;
-}
--- Empresas/Llamada.java
+++
@@ -1,4 +0,0 @@
-package Empresas;
-
-public class Llamada {
-}
