73e53669d3125bbe5330cf97f45c27478e28ab9b
tobiama <amaliagillig2508@gmail.com>
2025-06-17 23:24:55 -0300

    Add files via upload

---
+++ Almuerzos/Pedido.java
@@ -0,0 +1,99 @@
+package Almuerzos;
+import Autos.Fecha;
+import Libros.Persona;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Pedido {
+    private Fecha fechaCreacion;
+    ArrayList<Plato> platos;
+    Plato plato;
+    private PersonaAlmuerzo solicitante;
+    private Fecha fechaEntrega;
+    private String estadoEntrega;
+
+
+    public Pedido(int dia, int mes, int anio, String nombre, String apellido, String estadoEntrega) {
+        this.fechaCreacion = new Fecha(dia, mes, anio);
+        this.platos = new ArrayList<>();
+        this.plato = new Plato();
+        this.solicitante = new PersonaAlmuerzo(nombre, apellido);
+        this.fechaEntrega = new Fecha(dia, mes, anio);
+        this.estadoEntrega = estadoEntrega;
+    }
+
+    public Pedido() {
+        this.fechaCreacion = new Fecha(0, 0, 0);
+        this.platos = new ArrayList<>();
+        this.plato = new Plato();
+        this.solicitante = new PersonaAlmuerzo("", "");
+        this.solicitante = new PersonaAlmuerzo();
+        this.fechaEntrega = new Fecha(0, 0, 0);
+        this.estadoEntrega = "";
+
+    }
+
+
+    public Fecha getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(Fecha fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public Fecha getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    public void setFechaEntrega(Fecha fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    public String getEstadoEntrega() {
+        return estadoEntrega;
+    }
+
+    public void setEstadoEntrega(String estadoEntrega) {
+        this.estadoEntrega = estadoEntrega;
+    }
+
+    public PersonaAlmuerzo getSolicitante() {
+        return solicitante;
+    }
+
+    public void setSolicitante(PersonaAlmuerzo solicitante) {
+        this.solicitante = solicitante;
+    }
+
+    public ArrayList<Plato> getPlatos() {
+        return platos;
+    }
+
+    public void setPlatos(ArrayList<Plato> platos) {
+        this.platos= platos;
+    }
+
+    public Plato getPlato () {
+        return plato;
+    }
+
+    public void setPlato (Plato plato){
+        this.platos = platos;
+    }
+
+    public int calcularPorcentaje () {
+        int porcentajeTotal = 0;
+        int porcentaje = 0;
+        porcentaje = ((Profesor) this.solicitante).getPorcDescuento() / 100;
+        porcentajeTotal =
+        return porcentajeTotal;
+
+    }
+
+    public double calcularPrecioConDescuento (Plato plato, PersonaAlmuerzo solicitante){
+        int descuento = ((Profesor) this.solicitante).getPorcDescuento();
+        return plato.getPrecio() - (plato.getPrecio() * descuento / 100);
+    }
+}
---
+++ Almuerzos/Plato.java
@@ -0,0 +1,34 @@
+package Almuerzos;
+
+public class Plato {
+    private String nombre;
+    private double precio;
+
+
+    public Plato(String nombre, double precio){
+        this.nombre= nombre;
+        this.precio= precio;
+    }
+
+    public Plato(){
+        this.nombre= "";
+        this.precio= 0;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
