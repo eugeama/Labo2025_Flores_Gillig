51982e7270002417e7e0af744b9b6ea46737921a
tobiama <amaliagillig2508@gmail.com>
2025-06-17 23:21:11 -0300

    Delete Computadoras directory

--- Computadoras/CPU.java
+++
@@ -1,8 +0,0 @@
-package Computadoras;
-
-public class CPU extends Componente{
-
-    public CPU(String fabricante, String modelo, int precio, int stock){
-        super(fabricante, modelo, precio, stock);
-    }
-}
