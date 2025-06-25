4538858e689e11ac1df9ae7fff51a4e1aff582e7
tobiama <amaliagillig2508@gmail.com>
2025-06-24 23:14:22 -0300

    Update Perro.java

--- Perro.java
+++ Perro.java
@@ -1,11 +1,37 @@
 package Mascotas;
 
 public class Perro extends Mascota {
+    private int alegria;
+
     public Perro(String nombre, String duenio) {
         super(nombre, duenio);
+        this.alegria = 1;
     }
 
-    public String saludar() {
-        return "guau";
+    public void alimentar() {
+        alegria++;
+    }
+
+    public String saludar(String duenioQueSaluda) {
+        String saludoBase = "guau";
+        if (duenioQueSaluda.equals(getDuenio())) {
+            String saludo = "";
+            for (int i = 0; i < alegria; i++) {
+                saludo += saludoBase;
+                if (i < alegria - 1) {
+                    saludo += " ";
+                }
+            }
+            if (alegria > 1) {
+                alegria--;
+            }
+            return saludo;
+        } else {
+            return saludoBase.toUpperCase() + "!";
+        }
+    }
+
+    public String tipoDeMascota() {
+        return "Perro";
     }
 }
