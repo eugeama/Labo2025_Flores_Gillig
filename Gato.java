690e111ac7659db08a3abaa0d34fd57dfa8a5083
tobiama <amaliagillig2508@gmail.com>
2025-06-24 23:09:21 -0300

    Update Gato.java

--- Gato.java
+++ Gato.java
@@ -1,11 +1,38 @@
 package Mascotas;
 
 public class Gato extends Mascota {
+    private int alegria;
+
     public Gato(String nombre, String duenio) {
         super(nombre, duenio);
+        this.alegria = 1;
     }
 
-    public String saludar() {
-        return "guau";
+    public void alimentar() {
+        alegria++;
+    }
+
+    @Override
+    public String saludar(String duenioQueSaluda) {
+        String saludoBase = "miau";
+        if (duenioQueSaluda.equals(getDuenio())) {
+            String saludo = "";
+            for (int i = 0; i < alegria; i++) {
+                saludo += saludoBase;
+                if (i < alegria - 1) {
+                    saludo += " ";
+                }
+            }
+            if (alegria > 1) {
+                alegria--;
+            }
+            return saludo;
+        } else {
+            return saludoBase.toUpperCase() + "!";
+        }
+    }
+
+    public String tipoDeMascota() {
+        return "Gato";
     }
 }
