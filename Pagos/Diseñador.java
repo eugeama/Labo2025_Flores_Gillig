0158ebac7226b2198a912e72397c4a7fff369090
eugeama <amaliagillig2508@gmail.com>
2025-08-06 09:07:00 -0300

    ejercicio pagos

---
+++ Pagos/Diseñador.java
@@ -0,0 +1,76 @@
+package Pagos;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Diseñador {
+    private Tipo tipoD;
+    HashMap<String, Integer> proyectos = new HashMap<String, Integer>();
+
+
+    public Diseñador(String nombre, Tipo tipoD, HashMap<String, Integer> proyectos) {
+        this.tipoD = Tipo.IT;
+        this.proyectos = proyectos;
+    }
+
+    public Tipo getTipoD() {
+        return tipoD;
+    }
+
+    public void setTipoD(Tipo tipoD) {
+        this.tipoD = tipoD;
+    }
+
+    public HashMap<String, Integer> getProyectos() {
+        return proyectos;
+    }
+
+    public void setProyectos(HashMap<String, Integer> proyectos) {
+        this.proyectos = proyectos;
+    }
+
+    public double sueldoEmpleado() {
+        int sueldoS = 0;
+        double sueldoT = 0;
+        for (Map.Entry<String, Integer> proyectosD : proyectos.entrySet()) {
+            sueldoS = proyectosD.getValue();
+        }
+        sueldoT = sueldoS * tipoD.getComision();
+        return sueldoT;
+    }
+
+    public int proyecRealizados() {
+        int cantProyect = 0;
+        for (Map.Entry<String, Integer> proyectosD : proyectos.entrySet()) {
+            cantProyect = cantProyect + 1;
+        }
+        return cantProyect;
+    }
+
+    public int sueldoP(String proyectoE){
+        int sueldo= 0;
+        for (Map.Entry<String, Integer> proyectosD : proyectos.entrySet()) {
+            if(proyectoE.equals(proyectosD.getKey())) {
+                sueldo = proyectosD.getValue();
+            }
+        }
+        return sueldo;
+    }
+
+    public HashMap<String, String> sueldoDiscriminado(){
+        String nombre= "";
+        int sueldoDisc= 0;
+        double sueldoCom= 0;
+        String total= "";
+
+        HashMap<String, String>proyectoSueldo= new HashMap<>();
+        for (Map.Entry<String, Integer> proyectosD : proyectos.entrySet()) {
+            nombre= proyectosD.getKey();
+            sueldoDisc= proyectosD.getValue();
+            sueldoCom= sueldoDisc*tipoD.getComision();
+
+
+        }
+        return proyectoSueldo;
+    }
+}
