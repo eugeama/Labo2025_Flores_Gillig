b91c7376fe9a1e61f35600945f155a9649744ab6
tobiama <amaliagillig2508@gmail.com>
2025-08-18 12:12:31 -0300

    Update Plato.java

--- Calorias/Plato.java
+++ Calorias/Plato.java
@@ -1,13 +1,14 @@
 package Calorias;
 
 import java.util.HashSet;
+import java.util.Map;
 
 public class Plato {
     private String nombre;
-    private HashSet<String>ingredientes;
+    private HashMap<Ingredientes>;
     private int caloria;
 
-    public Plato(String nombre, HashSet<String>ingredientes, int caloria){
+    public Plato(String nombre, HashMap<Ingredientes>;, int caloria){
         this.nombre= nombre;
         this.ingredientes= ingredientes;
         this.caloria= caloria;
@@ -41,5 +42,66 @@ public class Plato {
 
     public void setCaloria(int caloria) {
         this.caloria = caloria;
+    }
+
+    public int calcularCalorias(){
+        int sumaCal=0;
+        for(Map.Entry<Ingrediente, integer> ingrediente:ingredientes.entryset()){
+            sumaCal+=ingrediente.getKey().getCaloria()*ingrediente.getValue();
+        }
+        return sumaCal;
+    }
+}package Calorias;
+
+import java.util.HashSet;
+import java.util.Map;
+
+public class Plato {
+    private String nombre;
+    private HashMap<Ingredientes>;
+    private int caloria;
+
+    public Plato(String nombre, HashMap<Ingredientes>;, int caloria){
+        this.nombre= nombre;
+        this.ingredientes= ingredientes;
+        this.caloria= caloria;
+    }
+
+    public Plato(){
+        this.nombre= "nombre";
+        this.ingredientes= new HashSet<>();
+        this.caloria= 0;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public HashSet<String> getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(HashSet<String> ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public int getCaloria() {
+        return caloria;
+    }
+
+    public void setCaloria(int caloria) {
+        this.caloria = caloria;
+    }
+
+    public int calcularCalorias(){
+        int sumaCal=0;
+        for(Map.Entry<Ingrediente, integer> ingrediente:ingredientes.entryset()){
+            sumaCal+=ingrediente.getKey().getCaloria()*ingrediente.getValue();
+        }
+        return sumaCal;
     }
 }
